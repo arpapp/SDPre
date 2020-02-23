2673d9e64d9e9d92b5021345c3aa4251da85062c
arpapp <trpapp@gmail.com>
2020-02-23 16:51:21 -0700

    added name

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -2,7 +2,7 @@ public class FirstProg
 {
   public static void main(String[] args)
   {
-    System.out.println("I am determined to learn how to code.");
+    System.out.println("I, Toni Papp, am determined to learn how to code.");
     System.out.println("Today's date is February 22nd and I am a bad typist.");
   }
 
