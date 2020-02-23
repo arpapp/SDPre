8240f53d08c85c3c5f8b8255d24ff80b6f6e843a
arpapp <trpapp@gmail.com>
2020-02-23 16:41:46 -0700

    finish jthw ex1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,9 @@
+public class FirstProg
+{
+  public static void main(String[] args)
+  {
+    System.out.println("I am determined to learn how to code.");
+    System.out.println("Today's date is February 22nd and I am a bad typist.");
+  }
+
+}
