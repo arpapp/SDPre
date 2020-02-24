fa2ea5a140eba2a7ca48e5e9f5566f57837fd190
arpapp <trpapp@gmail.com>
2020-02-23 20:42:33 -0700

    completed o1L3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3
+{
+  public static void main(String [] args)
+  {
+    System.out.println("Name : Antoinette Papp");
+    System.out.println("Age : 28");
+    System.out.println("Province : British Columbia");
+  }
+}
