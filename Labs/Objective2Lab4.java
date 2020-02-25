1a7aef9afc77655e8b1a0ba2d015451cc1a71697
arpapp <trpapp@gmail.com>
2020-02-24 21:47:31 -0700

    completed o2l4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,16 @@
+public class Objective2Lab4
+{
+  public static void main(String[]args)
+  {
+    double num1, num2, num3, average, sum;
+
+
+    num1 = 15;
+    num2 = 6;
+    num3 = 2;
+    sum = num1 + num2 + num3;
+    average = sum/3;
+
+    System.out.println(average);
+  }
+}
