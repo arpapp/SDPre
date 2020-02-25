b66069aa894856bf76f3edd5f57ba36d5fce809f
arpapp <trpapp@gmail.com>
2020-02-24 21:09:30 -0700

    completed o2l2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2
+{
+  public static void main(String []args)
+  {
+    String name = "Antoinette Papp";
+    int age = 28;
+    String province = "British Columbia";
+
+    System.out.print("Name: " + name  + "\t");
+    System.out.print("Age: " + age + "\t");
+    System.out.print("Province: " + province);
+    System.out.println();
+  }
+}
