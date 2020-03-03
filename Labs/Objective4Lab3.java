f164df02637efe7235450bfab573b63b9aa16f47
arpapp <trpapp@gmail.com>
2020-03-02 19:49:28 -0700

    o4l3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Objective4Lab3{
+  public static void main(String[]args){
+    Scanner keyboard = new Scanner(System.in);
+    int userNum;
+
+    System.out.println("Please enter a number: ");
+    userNum = keyboard.nextInt();
+
+    if (userNum > 0){
+      System.out.println("The number is positive.");
+    }
+    else if (userNum == 0){
+      System.out.println("The number is equal to 0.");
+    }
+    else if (userNum < 0){
+      System.out.println("The number is negative.");
+    }
+
+keyboard.close();
+  }
+}
