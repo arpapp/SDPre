53a1096dbe46742603c76fe3b8636818a092ef1b
arpapp <trpapp@gmail.com>
2020-03-05 22:45:43 -0700

    o7l3

---
+++ Labs/Objective7Lab3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Objective7Lab3{
+  public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+      while(true){
+        printMenu();
+        selection = scanner.nextInt();
+        if (selection == 1)
+        System.out.println("Hello World");
+      else if (selection == 2)
+        System.out.println("Fries, mangoes, crabs");
+      else if (selection ==3){
+        System.out.println("Goodbye.");
+        break;}
+        else if (selection>3)
+        System.out.println("ERROR");
+      }
+    }
+
+    public static void printMenu(){
+      System.out.println("____Menu____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List my favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+  }
+}
